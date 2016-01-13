dc646014aa8259ddaac6710a6a889dd756fc5289
Nooshin Sadat Mirzadeh <mirzadeh@parsapc14.epfl.ch>
2016-01-13 18:08:17 +0100

    added the new files

---
+++ benchmarks/data-analytics/master/start-ssh-serf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# start sshd
+echo "start sshd..."
+service ssh start
+
+# start sef
+echo -e "\nstart serf..." 
+/etc/serf/start-serf-agent.sh > serf_log &
+
+sleep 5
+
+serf members
