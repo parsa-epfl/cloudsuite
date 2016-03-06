fa1aa47038074ca15ecd8e3a72aad564c5130f48
Nooshin <nooshin.mirzadeh@epfl.ch>
2016-03-06 12:58:22 +0100

    Fix login bug

--- benchmarks/web-serving/run.sh
+++ benchmarks/web-serving/run.sh
@@ -4,7 +4,8 @@ docker exec faban_client sh -c "sed -i 's/<fa:scale.*/<fa:scale>7<\\/fa:scale>/'
 docker exec faban_client sh -c "sed -i 's/<fa:rampUp.*/<fa:rampUp>10<\\/fa:rampUp>/' /web20_benchmark/deploy/run.xml"
 docker exec faban_client sh -c "sed -i 's/<fa:rampDown.*/<fa:rampDown>10<\\/fa:rampDown>/' /web20_benchmark/deploy/run.xml"
 docker exec faban_client sh -c "sed -i 's/<fa:steadyState.*/<fa:steadyState>30<\\/fa:steadyState>/' /web20_benchmark/deploy/run.xml"
-docker exec faban_client sh -c "sed -i 's/<host.*/<host>web_server<\\/host>/' /web20_benchmark/deploy/run.xml"
+WEB_SERVER_IP=$(docker inspect --format '{{ .NetworkSettings.Networks.my_net.IPAddress }}' web_server)
+docker exec faban_client sh -c "sed -i 's/<host.*/<host>$WEB_SERVER_IP<\\/host>/' /web20_benchmark/deploy/run.xml"
 docker exec faban_client sh -c "sed -i 's/<port.*/<port>8080<\\/port>/' /web20_benchmark/deploy/run.xml"
 docker exec faban_client sh -c "sed -i 's/<outputDir.*/<outputDir>\/faban\/output<\\/outputDir>/' /web20_benchmark/deploy/run.xml"
 docker exec faban_client sh -c "cd /web20_benchmark && ant run"
