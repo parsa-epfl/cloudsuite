073c3dea895da7be5639ee20269c9703d6122f2f
ivonindza <ivonindza@gmail.com>
2016-05-20 10:35:48 +0200

    Add load scale option to faban client (web-serving) (#49)

--- benchmarks/web-serving/faban_client/bootstrap.sh
+++ benchmarks/web-serving/faban_client/bootstrap.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
+if [ $# -gt 2 ]; then
+  echo "Usage: faban_client WEB_SERVER_IP [LOAD_SCALE]"
+  exit 1
+fi
+
+if [ $# -lt 1 ]; then
   echo "Web server IP is a mandatory parameter."
   exit 1
 fi
+
 WEB_SERVER_IP=$1
+LOAD_SCALE=${2:-7}
 
 while [ "$(curl -sSI web_server:8080 | grep 'HTTP/1.1' | awk '{print $2}')" != "200" ]; do
   sleep 1
@@ -12,7 +19,7 @@ done
 
 /faban/master/bin/startup.sh
 cd /web20_benchmark/build && java -jar Usergen.jar http://${WEB_SERVER_IP}:8080
-sed -i "s/<fa:scale.*/<fa:scale>7<\\/fa:scale>/" /web20_benchmark/deploy/run.xml
+sed -i "s/<fa:scale.*/<fa:scale>${LOAD_SCALE}<\\/fa:scale>/" /web20_benchmark/deploy/run.xml
 sed -i "s/<fa:rampUp.*/<fa:rampUp>10<\\/fa:rampUp>/" /web20_benchmark/deploy/run.xml
 sed -i "s/<fa:rampDown.*/<fa:rampDown>10<\\/fa:rampDown>/" /web20_benchmark/deploy/run.xml
 sed -i "s/<fa:steadyState.*/<fa:steadyState>30<\\/fa:steadyState>/" /web20_benchmark/deploy/run.xml
