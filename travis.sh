5462c8fb341517e6014f45c3430f97d566eb5af9
sarora <somyarora96@gmail.com>
2016-06-06 19:43:55 +0200

    error corrected

--- travis.sh
+++ travis.sh
@@ -1,18 +1,17 @@
-  #!/bin/bash
-  path=$(git --no-pager diff --name-only ${TRAVIS_BRANCH} $(git merge-base ${TRAVIS_BRANCH} master))
-  paths=( $path )
-  counter=0
-  check1=”${DH_REPO#*/}”
-  check2=$IMG_TAG
+path=$(git --no-pager diff --name-only ${TRAVIS_BRANCH} $(git merge-base ${TRAVIS_BRANCH} master))
+paths=( $path )
+counter=0
+check1=”${DH_REPO#*/}”
+check2=$IMG_TAG
 
-  while [[ ${paths[counter]} ]]; 
-  do 
-  	benchmark="${paths[counter]#*/}"; 
+while [[ ${paths[counter]} ]]; 
+ do 
+ 	benchmark="${paths[counter]#*/}"; 
 	tag="${benchmark#*/}"; 
 	benchmark="${benchmark%%/*}"; 
 	tag="${tag%%/*}"; 
 
-	if [ “${check1}” -eq “${benchmark && “${check2}” -eq “${tag}” ]
+	if [ “${check1}” -eq “${benchmark} && “${check2}” -eq “${tag}” ]
 	    then
 		
 		 travis_wait 40 docker build -t $DH_REPO:$IMG_TAG $DF_PATH
@@ -24,4 +23,4 @@
 		fi
 	fi
 	let counter=counter+1; 
- done
+done
