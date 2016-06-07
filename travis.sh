8861293a072f2290b0718d5763cc5aa0e3bfa39c
sarora <somyarora96@gmail.com>
2016-06-07 19:22:19 +0200

    grep

--- travis.sh
+++ travis.sh
@@ -1,42 +1,35 @@
-set -x
-echo "TRAVIS_COMMIT_RANGE= ${TRAVIS_COMMIT_RANGE}"
 path=$(git --no-pager diff --name-only ${TRAVIS_COMMIT_RANGE})
-paths=( $path )
-counter=0
-check1=${DH_REPO#*/}
-check2=$IMG_TAG
-
-echo $path
-echo ${paths[counter]}
+should_be_built=0
+benchmark_name=${DH_REPO#*/}
+tag_name=$IMG_TAG
 
 if [ -z "$path" ]
    then
-	echo "No Modifications to this image"
+	echo "No Modifications required."
+else
+  echo "Checking against modified files"
 fi
 
-while [[ ${paths[counter]} ]];
- do
- 	benchmark="${paths[counter]#*/}";
-	tag="${benchmark#*/}";
-	benchmark="${benchmark%%/*}";
-	tag="${tag%%/*}";
+if grep -q "$benchmark_name/$tag_name" <<<$path || grep -q "$benchmark_name/latest" <<<$path
+   then
 
-	echo "Entered while"
+  travis_wait 40 docker build -t $DH_REPO:$IMG_TAG $DF_PATH
+  should_be_built=1
 
-	if [ "${check1}" = "${benchmark}" ] && [ "${check2}" = "${tag}" ]
-	    then
+  		 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]
+  		   then
 
-		 travis_wait 40 docker build -t $DH_REPO:$IMG_TAG $DF_PATH
+        docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
+  			travis_wait 40 docker push $DH_REPO
 
-		 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]
+      else
+        echo "No push command executed"
 
-		   then
-			docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
-			travis_wait 40 docker push $DH_REPO
+  		fi
 
-		fi
-	else
-			echo "No Modifications to this image"
-	fi
-	let counter=counter+1;
-done
+    fi
+
+  if [ $should_be_built -eq 0 ]
+     then
+    echo "No Modifications to this image"
+  fi
