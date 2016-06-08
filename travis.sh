a75702aa7f6e24184e462d312f5327820cb27a05
sarora <somyarora96@gmail.com>
2016-06-08 15:24:25 +0200

    return_value3

--- travis.sh
+++ travis.sh
@@ -15,12 +15,9 @@ fi
 if ( grep -q "$benchmark_name/$tag_name" <<<$modified_files ) || ( grep -q "$benchmark_name" <<<$modified_files && [ $tag_name = "latest" ] ) || ( grep -q "1travis.sh" <<<$modified_files ) || ( grep -q ".1travis.yml" <<<$modified_files )
    then
 
-  return_value=travis_wait 40 docker build -t $DH_REPO:$IMG_TAG $DF_PATH
-
-  echo "return_value"
-  echo $return_value
-
-  if [ $return_value -nq "0" ]
+  travis_wait 40 docker build -t $DH_REPO:$IMG_TAG $DF_PATH
+  
+  if [ result -eq 1 ]
     then
       TRAVIS_TEST_RESULT=1;
   fi
