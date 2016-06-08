33da1be9a5fedb3e6719a3b751a0986c84055711
sarora <somyarora96@gmail.com>
2016-06-08 17:11:31 +0200

    final

--- travis.sh
+++ travis.sh
@@ -1,54 +1,51 @@
-set -x
 modified_files=$(git --no-pager diff --name-only ${TRAVIS_COMMIT_RANGE})
-echo $modified_files
 benchmark_name=${DH_REPO#*/}
 tag_name=$IMG_TAG
-return_value=0;
 
 if [ -z "$modified_files" ]
-   then
-	   echo "No Modifications required."
+then
+    echo "No Modifications required."
 else
-     echo "Checking against modified files"
+    echo "Checking against modified files"
 fi
 
 if ( grep -q "$benchmark_name/$tag_name" <<<$modified_files ) ||
    ( grep -q "$benchmark_name" <<<$modified_files && [ $tag_name = "latest" ] ) ||
-   ( grep -q "1travis.sh" <<<$modified_files ) ||
-   ( grep -q ".1travis.yml" <<<$modified_files )
-   then
+   ( grep -q "travis.sh" <<<$modified_files ) ||
+   ( grep -q ".travis.yml" <<<$modified_files )
+then
 
-     travis_wait 40 docker build -t $DH_REPO:$IMG_TAG $DF_PATH
-     result=$?
+    travis_wait 40 docker build -t $DH_REPO:$IMG_TAG $DF_PATH
+    result=$?
 
     if [ $result -eq "1" ]
-      then
+    then
         return 1
     fi
 
     if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]
-      then
+    then
 
         docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
-
+​
         if [ $result -eq "1" ]
-          then
+        then
             return 1
         fi
 
   	    travis_wait 40 docker push $DH_REPO
 
         if [ $result -eq "1" ]
-          then
+        then
             return 1
         fi
-
+​
     else
         echo "No push command executed"
-
+​
   	fi
-
+​
 else
-  echo "No Modifications to this image"
-
+    echo "No Modifications to this image"
+​
 fi
