10ffb414373785b66c135295ae5a7c1ea8538344
sarora <somyarora96@gmail.com>
2016-06-21 14:13:01 +0200

    spellcheck first, merge later

--- docs.sh
+++ docs.sh
@@ -2,6 +2,32 @@
 MODIFIED_FILES=$(git --no-pager diff --name-only ${TRAVIS_COMMIT_RANGE})
 if grep -q "docs/" <<<$MODIFIED_FILES
 then
+  sudo apt-get install -y aspell-en
+  modified_files_arr=($MODIFIED_FILES)
+  rm -rf ./Misspelled_words.txt
+  for docs_file_modified in "${modified_files_arr[@]}"
+  do
+    if grep -q "docs/" <<<$docs_file_modified
+    then
+      echo "$docs_file_modified :- " > ./misspelled_per_file.txt;
+      <$docs_file_modified aspell pipe list -d en_US --encoding utf-8 --personal=./.aspell.en.pws |
+      grep '[a-zA-Z]\+ [0-9]\+ [0-9]\+' -oh |
+      grep '[a-zA-Z]\+' -o | sort | uniq |
+      while read word; do
+        grep -on "\<$word\>" $docs_file_modified;
+      done >>./misspelled_per_file.txt;
+      sort -n ./misspelled_per_file.txt -o ./misspelled_per_file.txt;
+      cat ./misspelled_per_file.txt >> ./Misspelled_words.txt;
+    fi
+  done
+  if [[ -s ./Misspelled_words.txt ]]
+  then
+    echo "~~~  Spelling Errors  ~~~ "
+    cat ./Misspelled_words.txt
+    return 1;
+  fi
+  rm -rf ./misspelled_word_per_file.txt
+  rm -rf ./Misspelled_words.txt
   mkdir out;
   cd out
   git clone https://github.com/ParsaLab/cloudsuite.git
@@ -30,30 +56,4 @@ then
   cd ${TRAVIS_BUILD_DIR}
   rm -rf out
   git checkout master
-  sudo apt-get install -y aspell-en
-  modified_files_arr=($MODIFIED_FILES)
-  rm -rf ./Misspelled_words.txt
-  for docs_file_modified in "${modified_files_arr[@]}"
-  do
-    if grep -q "docs/" <<<$docs_file_modified
-    then
-      echo "$docs_file_modified :- " > ./misspelled_per_file.txt;
-      <$docs_file_modified aspell pipe list -d en_US --encoding utf-8 --personal=./.aspell.en.pws |
-      grep '[a-zA-Z]\+ [0-9]\+ [0-9]\+' -oh |
-      grep '[a-zA-Z]\+' -o | sort | uniq |
-      while read word; do
-        grep -on "\<$word\>" $docs_file_modified;
-      done >>./misspelled_per_file.txt;
-      sort -n ./misspelled_per_file.txt -o ./misspelled_per_file.txt;
-      cat ./misspelled_per_file.txt >> ./Misspelled_words.txt;
-    fi
-  done
-  if [[ -s ./Misspelled_words.txt ]]
-  then
-    echo "~~~  Spelling Errors  ~~~ "
-    cat ./Misspelled_words.txt
-    return 1;
-  fi
-  rm -rf ./misspelled_word_per_file.txt
-  rm -rf ./Misspelled_words.txt
 fi
