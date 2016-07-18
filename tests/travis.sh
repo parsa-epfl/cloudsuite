d4bf725d92434ea8813b9685211fee2d18aa4078
srora <somyarora96@gmail.com>
2016-07-18 18:45:44 +0200

    Add comments to the Travis config file. (#66)

--- tests/travis.sh
+++ tests/travis.sh
@@ -1,5 +1,8 @@
+# @authors: Somya Arora, Arash Pourhabibi
 #!/bin/bash
+# Figure out the modified files in this push command or pull request
 modified_files=$(git --no-pager diff --name-only ${TRAVIS_COMMIT_RANGE})
+# Get benchmark name and tag name for this build
 benchmark_name=${DH_REPO#*/}
 tag_name=$IMG_TAG
 if [ -z "$modified_files" ]
@@ -8,25 +11,31 @@ then
 else
     echo "Checking against modified files"
 fi
+# Find out whether the files related with the current build were modified or not 
 if ( grep -q "$benchmark_name/$tag_name" <<<$modified_files ) ||
    ( grep -q "$benchmark_name" <<<$modified_files && [ $tag_name = "latest" ] ) ||
    ( grep -q "travis.sh" <<<$modified_files ) ||
    ( grep -q ".travis.yml" <<<$modified_files )
 then
+    # if modified, then rebuild their docker image
     travis_wait 40 docker build -t $DH_REPO:$IMG_TAG $DF_PATH
+    #make sure build was successful
     result=$?
     if [ $result -eq "1" ]
     then
         return 1
     fi
+    # Push if this file was triggerred by a push command (not a pull request)
     if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]
     then
         docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
+        # Pushing needs login, test if login was successful
         result=$?
         if [ $result -eq "1" ]
         then
             return 1
         fi
+        # Push if it logged in, test if push was successful
   	    travis_wait 40 docker push $DH_REPO
         result=$?
         if [ $result -eq "1" ]
@@ -36,6 +45,7 @@ then
     else
         echo "No push command executed"
   	fi
+# if no file related to this image was modified
 else
     echo "No Modifications to this image"
 fi
