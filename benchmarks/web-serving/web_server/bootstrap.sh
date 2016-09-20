2fa6f8c2733febdf07efdf79e18bd2b35be287f6
Nooshin <nooshin.mirzadeh@epfl.ch>
2016-09-20 19:17:18 +0200

    Fix the 500 error

--- benchmarks/web-serving/web_server/bootstrap.sh
+++ benchmarks/web-serving/web_server/bootstrap.sh
@@ -4,7 +4,7 @@
 DB_SERVER_IP=${1:-"mysql_server"}
 MEMCACHE_SERVER_IP=${2:-"memcache_server"}
 sed -i -e"s/mysql_server/${DB_SERVER_IP}/" elgg/engine/settings.php
-sed -i -e"s/memcache_server/${MEMCACHE_SERVER_IP}/" elgg/engine/settings.php
+sed -i -e"s/'memcache_server'/'${MEMCACHE_SERVER_IP}'/" elgg/engine/settings.php
 
 
 FPM_CHILDREN=${3:-80}
