c292989d0a0eeca11d1087a389a5c0550f3348d0
Rahul Sharma <r.sharma@epfl.ch>
2018-06-28 17:11:31 +0200

    web-serving uses faban common image

--- benchmarks/web-serving/faban_client/bootstrap.sh
+++ benchmarks/web-serving/faban_client/bootstrap.sh
@@ -17,8 +17,8 @@ while [ "$(curl -sSI ${WEB_SERVER_IP}:8080 | grep 'HTTP/1.1' | awk '{print $2}')
   sleep 1
 done
 
-sed -i -e"s/num_users=500/num_users=${LOAD_SCALE}/" /faban/usersetup.properties
-/faban/master/bin/startup.sh
+sed -i -e"s/num_users=500/num_users=${LOAD_SCALE}/" $FABAN_HOME/usersetup.properties
+$FABAN_HOME/master/bin/startup.sh
 cd /web20_benchmark/build && java -jar Usergen.jar http://${WEB_SERVER_IP}:8080
 sed -i "s/<fa:scale.*/<fa:scale>${LOAD_SCALE}<\\/fa:scale>/" /web20_benchmark/deploy/run.xml
 sed -i "s/<fa:rampUp.*/<fa:rampUp>10<\\/fa:rampUp>/" /web20_benchmark/deploy/run.xml
@@ -26,7 +26,7 @@ sed -i "s/<fa:rampDown.*/<fa:rampDown>10<\\/fa:rampDown>/" /web20_benchmark/depl
 sed -i "s/<fa:steadyState.*/<fa:steadyState>30<\\/fa:steadyState>/" /web20_benchmark/deploy/run.xml
 sed -i "s/<host.*/<host>${WEB_SERVER_IP}<\\/host>/" /web20_benchmark/deploy/run.xml
 sed -i "s/<port.*/<port>8080<\\/port>/" /web20_benchmark/deploy/run.xml
-sed -i "s/<outputDir.*/<outputDir>\/faban\/output<\\/outputDir>/" /web20_benchmark/deploy/run.xml
+sed -i "s@<outputDir.*@<outputDir>${FABAN_HOME}\/output<\\/outputDir>@" /web20_benchmark/deploy/run.xml
 cd /web20_benchmark && ant run
-cat /faban/output/*/summary.xml
+cat $FABAN_HOME/output/*/summary.xml
 
