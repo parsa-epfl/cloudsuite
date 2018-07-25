ec0b366aa20c7bb9417e092de4174681dfca6551
Rahul Sharma <rahul.kenu@gmail.com>
2018-07-25 19:09:01 +0200

    Updated index-generator

--- benchmarks/web-search/index_generator/docker-entrypoint.sh
+++ benchmarks/web-search/index_generator/docker-entrypoint.sh
@@ -4,6 +4,8 @@ if [ "$#" -ne 1 ]; then
     echo "usage: Command no_pages"
     exit 1
 fi
+NUM_SERVERS=1
+SERVER_HEAP_SIZE=12g
 cd ~
 NO_PAGES=$1
 DUMPNAME=dump_$NO_PAGES
@@ -15,9 +17,12 @@ head -c $BYTE_OFFSET ~/wiki_dump/enwiki-latest-pages-articles-multistream.xml.bz
 bzip2 -dc $DUMPNAME.xml.bz2 > wiki_dump.xml
 echo "</mediawiki>" >> wiki_dump.xml
 
+$SOLR_HOME/bin/solr start -cloud -p $SOLR_PORT -s $SOLR_CORE_DIR -m $SERVER_HEAP_SIZE 
+$SOLR_HOME/bin/solr status
+$SOLR_HOME/bin/solr create_collection -c cloudsuite_web_search -d _default -shards $NUM_SERVERS -p $SOLR_PORT
 
-"/home/solr/solr-7.4.0/bin/solr" start -p 8983 -s "solr-7.4.0/example/example-DIH/solr"
-sleep 2
+
+
 
 curl "http://localhost:8983/solr/cloudsuite_web_search/dataimport?command=full-import"
 
