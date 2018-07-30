b9170a09e442d5e863fb10250705a95cc154e449
Xinliang Liu <xinliang.liu@linaro.org>
2018-07-30 20:31:23 +0800

    Refine data caching graph

--- test_data_caching.sh
+++ test_data_caching.sh
@@ -26,12 +26,25 @@ docker rm dc-server4 || true
 docker rm dc-client || true
 
 docker run --cpuset-cpus="0-3" --name dc-server1 --net caching_network -d cloudsuite/data-caching:server
-docker run --cpuset-cpus="4-7" --name dc-server2 --net caching_network -d cloudsuite/data-caching:server
+docker run --cpuset-cpus="16-19" --name dc-server2 --net caching_network -d cloudsuite/data-caching:server
 
-docker run -it --name dc-client --net caching_network cloudsuite/data-caching:client bash -c "cd /usr/src/memcached/memcached_client/; \
+docker run --cpuset-cpus="32-47" -d --name dc-client --net caching_network cloudsuite/data-caching:client bash -c 'cd /usr/src/memcached/memcached_client/; \
 echo dc-server1, 11211 > docker_servers.txt; \
 echo dc-server2, 11211 >> docker_servers.txt; \
-./loader -a ../twitter_dataset/twitter_dataset_unscaled -o ../twitter_dataset/twitter_dataset_30x -s docker_servers.txt -w 4 -S 30 -D 4096 -j -T 1;" > output.txt
+./loader -a ../twitter_dataset/twitter_dataset_unscaled -o ../twitter_dataset/twitter_dataset_30x -s docker_servers.txt -w 4 -S 30 -D 4096 -j -T 1; \
+for i in `seq 1000 26000 226000`; do \
+	echo rps: $i; \
+	./loader -a ../twitter_dataset/twitter_dataset_30x -s docker_servers.txt -g 0.8 -T 20 -c 200 -w 8 -e -r $i & \
+	for j in  {1..12}j; do \
+		mpstat -P ALL 20 1; \
+	done; \
+	pkill loader; \
+done'
 
+
+#while docker ps | grep 'dc-client'; do
+#	echo Still waiting for scale and warmup...
+#	sleep 10
+#done
 
 #./loader -a ../twitter_dataset/twitter_dataset_30x -s docker_servers.txt -g 0.8 -T 1 -c 200 -w 8" > output.txt
