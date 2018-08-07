9c7e6af60ba58f00c1591787527ec9eba860e608
Xinliang Liu <xinliang.liu@linaro.org>
2018-08-07 16:56:54 +0800

    Final graph script

--- test_data_caching.sh
+++ test_data_caching.sh
@@ -32,12 +32,12 @@ docker run --cpuset-cpus="32-47" -d --name dc-client --net caching_network cloud
 echo dc-server1, 11211 > docker_servers.txt; \
 echo dc-server1, 11211 > docker_servers.txt; \
 ./loader -a ../twitter_dataset/twitter_dataset_unscaled -o ../twitter_dataset/twitter_dataset_30x -s docker_servers.txt -w 4 -S 30 -D 4096 -j -T 1; \
-for i in `seq 98000 1000 160000`; do \
+for i in `seq 10000 500 155000`; do \
 	echo rps: $i; \
-	./loader -a ../twitter_dataset/twitter_dataset_30x -s docker_servers.txt -g 0.8 -T 20 -c 200 -w 8 -e -r $i & \
-	sleep 600; \
-	for j in  {1..3}j; do \
-		mpstat -P ALL 200 1; \
+	./loader -a ../twitter_dataset/twitter_dataset_30x -s docker_servers.txt -g 0.8 -T 50 -c 200 -w 8 -e -r $i & \
+	sleep 355; \
+	for j in  {1..2}j; do \
+		mpstat -P ALL 25 1; \
 	done; \
 	pkill loader; \
 done'
