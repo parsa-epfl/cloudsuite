1cdb8f740910d43984823235a38bfcd5572d3fbf
aasgari <aasgari@node.dc-benchmark.cloudsuite3-pg0.utah.cloudlab.us>
2018-08-09 07:44:38 -0600

    Graph on Utah 8 core machine

--- test_data_caching.sh
+++ test_data_caching.sh
@@ -25,17 +25,17 @@ docker rm dc-server4 || true
 
 docker rm dc-client || true
 
-docker run --cpuset-cpus="16-19" --name dc-server1 --net caching_network -d cloudsuite/data-caching:server
-#docker run --cpuset-cpus="16-19" --name dc-server2 --net caching_network -d cloudsuite/data-caching:server
+docker run --cpuset-cpus="0-3" --name dc-server1 --net caching_network -d cloudsuite/data-caching:server
+#docker run --cpuset-cpus="4-7" --name dc-server2 --net caching_network -d cloudsuite/data-caching:server
 
-docker run --cpuset-cpus="32-47" -d --name dc-client --net caching_network cloudsuite/data-caching:client bash -c 'cd /usr/src/memcached/memcached_client/; \
+docker run --cpuset-cpus="4-7" -d --name dc-client --net caching_network cloudsuite/data-caching:client bash -c 'cd /usr/src/memcached/memcached_client/; \
 echo dc-server1, 11211 > docker_servers.txt; \
 echo dc-server1, 11211 > docker_servers.txt; \
 ./loader -a ../twitter_dataset/twitter_dataset_unscaled -o ../twitter_dataset/twitter_dataset_30x -s docker_servers.txt -w 4 -S 30 -D 4096 -j -T 1; \
 for i in `seq 10000 500 155000`; do \
 	echo rps: $i; \
-	./loader -a ../twitter_dataset/twitter_dataset_30x -s docker_servers.txt -g 0.8 -T 50 -c 200 -w 8 -e -r $i & \
-	sleep 355; \
+	./loader -a ../twitter_dataset/twitter_dataset_30x -s docker_servers.txt -g 0.8 -T 50 -c 200 -w 4 -e -r $i & \
+	sleep 300; \
 	for j in  {1..2}j; do \
 		mpstat -P ALL 25 1; \
 	done; \
