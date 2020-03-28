223a49a7f5e49fc54278cb29b7ead8093cb8135b
Alok <alok123thatikunta@gmail.com>
2020-03-28 18:32:35 +0000

    Update mysql bind-address

--- commons/mysql/files/execute.sh
+++ commons/mysql/files/execute.sh
@@ -7,11 +7,11 @@ service mysql restart
 # Wait for mysql to come up
 while :; do
     mysql -uroot -p${root_password} -e "status" && break
-    sleep 1
+    sleep 10
 done
 
 mysql -uroot -p$root_password -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$root_password' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
 service mysql stop
 
-/usr/sbin/mysqld
+/usr/sbin/mysqld --user=root
