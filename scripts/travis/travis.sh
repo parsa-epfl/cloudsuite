c74f15e893039df014c8efd2ee8318aceae5b191
Alok Thatikunta <alok123thatikunta@gmail.com>
2020-05-12 15:46:35 -0400

    Make travis docker push back to normal

--- scripts/travis/travis.sh
+++ scripts/travis/travis.sh
@@ -15,20 +15,31 @@ if (grep -q "$benchmark_name/$tag_name" <<<$modified_files) ||
     (grep -q "$benchmark_name" <<<$modified_files && [ $tag_name = "latest" ]) ||
     (grep -q "travis.sh" <<<$modified_files) ||
     (grep -q ".travis.yml" <<<$modified_files); then
+    # if modified, then rebuild their docker image
+    travis_wait 40 docker buildx build --platform linux/amd64,linux/arm64,linux/riscv64 -t $DH_REPO:$IMG_TAG $DF_PATH
+    # make sure build was successful
+    result=$?
+    if [ $result != "0" ]; then
+        return 1
+    fi
     # Push if this file was triggerred by a push command (not a pull request)
-    docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
-    # Pushing needs login, test if login was successful
-    result=$?
-    if [ $result != "0" ]; then
-        return 1
+    if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+        docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
+        # Pushing needs login, test if login was successful
+        result=$?
+        if [ $result != "0" ]; then
+            return 1
+        fi
+        # push the docker image
+        travis_wait 40 docker buildx build --platform linux/amd64,linux/arm64,linux/riscv64 -t $DH_REPO:$IMG_TAG --push $DF_PATH
+        result=$?
+        if [ $result != "0" ]; then
+            return 1
+        fi
+    else
+        echo "No push command executed"
     fi
-    # if modified, then rebuild annd push their docker image
-    travis_wait 40 docker buildx build --platform linux/amd64,linux/arm64,linux/riscv64 -t $DH_REPO:$IMG_TAG --push $DF_PATH
-    #make sure build was successful
-    result=$?
-    if [ $result != "0" ]; then
-        return 1
-    fi
+
 # if no file related to this image was modified
 else
     echo "No Modifications to this image"
