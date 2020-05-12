1bbe2fdec4255df126211ba3eba9808feb35c54f
Alok Thatikunta <alok123thatikunta@gmail.com>
2020-05-12 16:29:50 -0400

    remove travis_wait

--- scripts/travis/travis.sh
+++ scripts/travis/travis.sh
@@ -16,7 +16,7 @@ if (grep -q "$benchmark_name/$tag_name" <<<$modified_files) ||
     (grep -q "travis.sh" <<<$modified_files) ||
     (grep -q ".travis.yml" <<<$modified_files); then
     # if modified, then rebuild their docker image
-    travis_wait 40 docker buildx build --platform linux/amd64,linux/arm64,linux/riscv64 -t $DH_REPO:$IMG_TAG $DF_PATH
+    docker buildx build --platform linux/amd64,linux/arm64,linux/riscv64 -t $DH_REPO:$IMG_TAG $DF_PATH
     echo "Buildx completed"
     # make sure build was successful
     result=$?
@@ -34,7 +34,7 @@ if (grep -q "$benchmark_name/$tag_name" <<<$modified_files) ||
             return 1
         fi
         # push the docker image
-        travis_wait 40 docker buildx build --platform linux/amd64,linux/arm64,linux/riscv64 -t $DH_REPO:$IMG_TAG --push $DF_PATH
+        docker buildx build --platform linux/amd64,linux/arm64,linux/riscv64 -t $DH_REPO:$IMG_TAG --push $DF_PATH
         result=$?
         if [ $result != "0" ]; then
             return 1
