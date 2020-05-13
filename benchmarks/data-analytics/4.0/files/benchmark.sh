45050887a39396cb7920cf7ffab50c74508fb553
komal1204 <41997541+komal1204@users.noreply.github.com>
2020-05-13 05:21:28 -0700

    Update data-analytics to v4.0 and hadoop to 2.9.2 (#238)
    * MultiVM for data-analytics, update hadoop to 2.9.2
    * Update data-analytics.md
    
    Co-authored-by: Ubuntu <ubuntu@datacaching.cloud.compas.cs.stonybrook.edu>
    Co-authored-by: Alok Thatikunta <alok123thatikunta@gmail.com>
    Co-authored-by: Vamshi Muthineni <vamshimuthinenii@gmail.com>

--- benchmarks/data-analytics/4.0/files/benchmark.sh
+++ benchmarks/data-analytics/4.0/files/benchmark.sh
@@ -3,13 +3,15 @@
 RED='\033[0;31m'
 RESET='\033[0m'
 
+export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))
+
 ${HADOOP_PREFIX}/bin/hdfs dfs -test -e /user/root/wiki
 if [ $? -ne 0 ]; then
   ${HADOOP_PREFIX}/bin/hdfs dfs -mkdir -p /user/root
   ${HADOOP_PREFIX}/bin/hdfs dfs -put /root/wiki /user/root/
 fi
 
-START=$(($(date +"%s%N") / 1000000))
+START=$(($(date +"%s%N")/1000000))
 
 # Create sequence files from wiki
 echo -e "${RED}Mahout: seqwiki${RESET}"
@@ -20,7 +22,7 @@ ${MAHOUT_HOME}/bin/mahout seq2sparse -i /user/root/wiki-seq -o /user/root/wiki-v
 # Create training and holdout sets with a random 80-20 split of the generated vector dataset
 echo -e "${RED}Mahout: split${RESET}"
 ${MAHOUT_HOME}/bin/mahout split -i /user/root/wiki-vectors/tfidf-vectors --trainingOutput /user/root/training \
-  --testOutput /user/root/testing -rp 20 -ow -seq -xm sequential
+                                --testOutput /user/root/testing -rp 20 -ow -seq -xm sequential
 # Train Bayes model
 echo -e "${RED}Mahout: trainnb${RESET}"
 ${MAHOUT_HOME}/bin/mahout trainnb -i /user/root/training -o /user/root/model -li /user/root/labelindex -ow -c
@@ -28,6 +30,6 @@ ${MAHOUT_HOME}/bin/mahout trainnb -i /user/root/training -o /user/root/model -li
 echo -e "${RED}Mahout: testnb${RESET}"
 ${MAHOUT_HOME}/bin/mahout testnb -i /user/root/testing -m /user/root/model -l /user/root/labelindex -ow -o /user/root/output -seq
 
-END=$(($(date +"%s%N") / 1000000))
+END=$(($(date +"%s%N")/1000000))
 TIME=$(($END - $START))
 echo -e "\nBenchmark time: ${TIME}ms"
