6839dffe0f74ee7f2f61f0070bf7a6204e809658
Vamshi Muthineni <vamshimuthinenii@gmail.com>
2020-05-29 03:42:11 +0000

    solr version changed to 6.6.6

--- benchmarks/web-search/server/docker-entrypoint.sh
+++ benchmarks/web-search/server/docker-entrypoint.sh
@@ -5,9 +5,9 @@ export SERVER_HEAP_SIZE=$1 &&
   export NUM_SERVERS=$2
 
 #Prepare Solr
-$SOLR_HOME/bin/solr start -cloud -p $SOLR_PORT -s $SOLR_CORE_DIR -m $SERVER_HEAP_SIZE
+$SOLR_HOME/bin/solr start -force -cloud -p $SOLR_PORT -s $SOLR_CORE_DIR -m $SERVER_HEAP_SIZE
 $SOLR_HOME/bin/solr status
-$SOLR_HOME/bin/solr create_collection -c cloudsuite_web_search -d basic_configs -shards $NUM_SERVERS -p $SOLR_PORT
+$SOLR_HOME/bin/solr create_collection -force -c cloudsuite_web_search -d basic_configs -shards $NUM_SERVERS -p $SOLR_PORT
 kill -9 $(pgrep java) $(pgrep java)
 
 cd $SOLR_CORE_DIR/cloudsuite_web_search*
@@ -20,4 +20,4 @@ echo "Index Node IP Address: "$(hostname -I)
 echo "================================="
 
 #Run Solr
-$SOLR_HOME/bin/solr start -cloud -f -p $SOLR_PORT -s $SOLR_CORE_DIR -m $SERVER_HEAP_SIZE
+$SOLR_HOME/bin/solr start -force -cloud -f -p $SOLR_PORT -s $SOLR_CORE_DIR -m $SERVER_HEAP_SIZE
