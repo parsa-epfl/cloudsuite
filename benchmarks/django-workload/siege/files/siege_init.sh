b40aa01c57c1b1ea662c4877c5dce31376125b46
Ubuntu <ajaygk95@gmail.com>
2020-09-09 02:48:28 +0000

    Siege fix

--- benchmarks/django-workload/siege/files/siege_init.sh
+++ benchmarks/django-workload/siege/files/siege_init.sh
@@ -6,5 +6,5 @@ chown -R tester:tester /tmp
 echo "Running with $SIEGE_WORKERS Siege workers"
 su - tester -c "cd /home/tester/django-workload/client                     \
                 && sed -i 's/localhost/$TARGET_ENDPOINT/g' urls.txt        \
+                && chmod 777 /home/tester/django-workload/client/urls.txt  \
                 && LOG='/tmp/siege.log' WORKERS=$SIEGE_WORKERS ./run-siege"
-
