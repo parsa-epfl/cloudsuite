ba54498fb500e0dca7fbfdeda4b5e2446abf926b
xusine <xusine@outlook.com>
2021-08-11 15:31:51 +0200

    Change base-os repo name

--- commons/base-os/build.sh
+++ commons/base-os/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for arch in amd64 arm64 riscv64; do
-    docker buildx build --platform=linux/${arch} -t cloudsuite/debian:${arch} -f Dockerfile.${arch} --push .
+    docker buildx build --platform=linux/${arch} -t cloudsuite/base-os:${arch} -f Dockerfile.${arch} --push .
 done
 
-docker manifest create --amend cloudsuite/debian:base-os cloudsuite/debian:amd64 cloudsuite/debian:arm64 cloudsuite/debian:riscv64
-docker manifest push cloudsuite/debian:base-os
+docker manifest create --amend cloudsuite/base-os:debian cloudsuite/base-os:amd64 cloudsuite/base-os:arm64 cloudsuite/base-os:riscv64
+docker manifest push cloudsuite/base-os:debian
