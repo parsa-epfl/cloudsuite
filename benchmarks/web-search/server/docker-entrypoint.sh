9c198a5e86cedd25b78ec4dca912da2c90bd79fb
aansaarii <ali.ansari@epfl.ch>
2022-08-09 10:55:46 +0200

    update solr to 9.0.0

--- benchmarks/web-search/server/docker-entrypoint.sh
+++ benchmarks/web-search/server/docker-entrypoint.sh
@@ -7,13 +7,21 @@ export SERVER_HEAP_SIZE=$1 &&
 #Prepare Solr
 $SOLR_HOME/bin/solr start -force -cloud -p $SOLR_PORT -s $SOLR_CORE_DIR -m $SERVER_HEAP_SIZE
 $SOLR_HOME/bin/solr status
-$SOLR_HOME/bin/solr create_collection -force -c cloudsuite_web_search -d basic_configs -shards $NUM_SERVERS -p $SOLR_PORT
+#$SOLR_HOME/bin/solr create_collection -force -c cloudsuite_web_search -d basic_configs -shards $NUM_SERVERS -p $SOLR_PORT
+#$SOLR_HOME/bin/solr create_collection -force -c cloudsuite_web_search -d sample_techproducts_configs -shards $NUM_SERVERS -p $SOLR_PORT
+#$SOLR_HOME/bin/solr create_collection -force -c cloudsuite_web_search -d _default -shards $NUM_SERVERS -p $SOLR_PORT
+
+
+$SOLR_HOME/bin/solr create_collection -force -c cloudsuite_web_search -d cloudsuite_web_search -shards $NUM_SERVERS -p $SOLR_PORT
+
 kill -9 $(pgrep java) $(pgrep java)
 
 cd $SOLR_CORE_DIR/cloudsuite_web_search*
 rm -rf data
 # Copy data from dataset to server
-cp --verbose -a /download/data .
+#cp --verbose -a /download/data .
+cp --verbose -a /root/data .
+#rm -r /download/*
 
 echo "================================="
 echo "Index Node IP Address: "$(hostname -I)
