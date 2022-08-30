0380434f3bfc019e5c53cdfc40045135b7868d99
aansaarii <ali.ansari@epfl.ch>
2022-08-30 10:11:52 +0200

    backup the changes to web-serving

--- benchmarks/web-serving/db_server/entrypoint.sh
+++ benchmarks/web-serving/db_server/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]; then
-  echo "Web server IP is a mandatory parameter."
-  exit 1
-fi
+# We no longer need this parameter
+#if [ $# -lt 1 ]; then
+#  echo "Web server IP is a mandatory parameter."
+#  exit 1
+#fi
 
+# don't download the database here, move it to the Dockerfile
 #wget --no-check-certificate -O /elgg_bigDB.dump https://cloudsuite.ch/download/web-serving/elgg_bigDB.dump
 
-WEB_SERVER_IP=$1
+#WEB_SERVER_IP=$1
 
 # workaround for overlayfs:
 # https://docs.docker.com/engine/userguide/storagedriver/overlayfs-driver/#limitations-on-overlayfs-compatibility
@@ -24,17 +26,31 @@ else
 	MY_SQL="mysql"
 fi
 
-service $MY_SQL restart
+#service $MY_SQL restart
 
 # Wait for mysql to come up
-while :; do $MY_SQL -uroot -p${root_password} -e "status" && break; sleep 1; done
-$MY_SQL -uroot -p$root_password -e "create database ELGG_DB;"
+#while :; do $MY_SQL -uroot -p${root_password} -e "status" && break; sleep 1; done
+#$MY_SQL -uroot -p$root_password -e "create database ELGG_DB;"
 
 # Need bash -c for redirection
-bash -c "$MY_SQL -uroot -p$root_password ELGG_DB < /elgg_bigDB.dump"
-$MY_SQL -uroot -p$root_password -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$root_password' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+#bash -c "$MY_SQL -uroot -p$root_password ELGG_DB < /elgg_bigDB.dump"
+#$MY_SQL -uroot -p$root_password -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$root_password' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 service $MY_SQL stop
 
-rm /elgg_bigDB.dump
+#rm /elgg_bigDB.dump
 
-/usr/sbin/${MY_SQL}d
+
+
+rm -rf /var/lib/mysql/*
+
+mariabackup --prepare --target-dir=/backup/
+
+mariabackup --copy-back --target-dir=/backup/
+
+chown -R mysql:mysql /var/lib/mysql/
+
+
+#don't start the service here, start it as CMD in the Dockerfile
+#/usr/sbin/mysqld
+service mysql start
+bash
