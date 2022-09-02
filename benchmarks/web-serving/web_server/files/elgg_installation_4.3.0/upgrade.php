7806eb372bf070454ac04d17f1620e8d0de8b50e
aansaarii <ali.ansari@epfl.ch>
2022-09-02 18:53:45 +0200

    move Elgg installation to a remote link

--- benchmarks/web-serving/web_server/files/elgg_installation_4.3.0/upgrade.php
+++
@@ -1,8 +0,0 @@
-<?php
-$autoload_path = __DIR__ . '/vendor/autoload.php';
-$autoload_available = include_once($autoload_path);
-if (!$autoload_available) {
-	die("Couldn't include '$autoload_path'. Did you run `composer install`?");
-}
-
-\Elgg\Application::upgrade();
