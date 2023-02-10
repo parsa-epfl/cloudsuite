6b0100969160cc31f08dbccad3dfd8d1e666dd6e
Cyan Lin <shanqing.lin@epfl.ch>
2023-02-10 14:21:48 +0100

    Change Base Image to Ubuntu 22.04 (#370)
    * Redirect to cloudsuitetest
    
    * Fix: memcached build result
    
    * Update the sbt for future new Scala version
    
    * Update MariaDB and Siege
    
    * Define JAVA_HOME in script
    
    * Fix the database service name
    
    * Add javahome for web-search
    
    * Rename docker's tag and remove old container
    
    * Remove old version of mysql
    
    * Fix base image building
    
    * Remove graphite
    
    * Fix the CI deprecated warning.

--- commons/base-os/build.sh
+++ commons/base-os/build.sh
@@ -4,5 +4,4 @@ for arch in amd64 arm64 riscv64; do
     docker buildx build --platform=linux/${arch} -t cloudsuite/base-os:${arch} -f Dockerfile.${arch} --push .
 done
 
-docker manifest create --amend cloudsuite/base-os:debian cloudsuite/base-os:amd64 cloudsuite/base-os:arm64 cloudsuite/base-os:riscv64
-docker manifest push cloudsuite/base-os:debian
+docker buildx imagetools create -t cloudsuite/base-os:ubuntu cloudsuite/base-os:amd64 cloudsuite/base-os:arm64 cloudsuite/base-os:riscv64
--- commons/mysql/mariadb-10.6/files/execute.sh
+++ commons/mysql/mariadb-10.6/files/execute.sh
@@ -2,7 +2,14 @@
 set -x
 set root_password=$1
 
-service mysql restart
+MY_SQL=$(find /etc/init.d -name "*mariadb*")
+if [ $MY_SQL ]; then
+	MY_SQL="mariadb"
+else
+	MY_SQL="mysql"
+fi
+
+service $MY_SQL restart
 
 # Wait for mysql to come up
 while :; do mysql -uroot -p${root_password} -e "status" && break; sleep 1; done
@@ -11,6 +18,6 @@ while :; do mysql -uroot -p${root_password} -e "status" && break; sleep 1; done
 
 mysql -uroot -p$root_password -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$root_password' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
-service mysql stop 
+service $MY_SQL stop 
 
 /usr/sbin/mysqld
